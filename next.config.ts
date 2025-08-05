e9500d909c9ee987459136c11425a2afc42065e0
Morgan Stovold <morgannstovold.dev@gmail.com>
2025-08-05 13:54:33 -0700

    Add redirect from /request-consultation to /contact in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/request-consultation",
+        destination: "/contact",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
